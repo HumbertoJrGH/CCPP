008bce13109f73ac3904db0717e62c94d747f6eb
HumbertoJrGH <dev.humbertojr@gmail.com>
2024-05-15 14:24:50 -0300

    correção parcial classe lista/celula/player

--- sysbase/character.cpp
+++ sysbase/character.cpp
@@ -8,6 +8,92 @@ class Character
 public:
 	Character()
 	{
-		cout << "defina o nome do seu personagem:";
+		// cout << "defina o nome do seu personagem:" << endl;
+	}
+};
+
+class Player
+{
+private:
+	string name;
+	int id;
+
+public:
+	Player(int prev = 0)
+	{
+		// Character();
+		this->id = prev + 1;
+	}
+	void setName(string n) { name = n; }
+	int getID() const { return id; }
+	string getName() const { return name; }
+	void printName() const { cout << name << endl; }
+	void printId() { cout << hex << uppercase << this->id << endl; }
+	virtual void teste() { cout << "faça seu teste" << endl; }
+};
+
+// CELULA DUPLA
+class Celula
+{
+private:
+	Player p;
+
+public:
+	Celula *next;
+	Celula *prev;
+	Celula(Player p) {		
+		this->next = this->prev = nullptr;
+		this->p = p;
+	}
+	Celula()
+	{
+		this->next = this->prev = nullptr;
+	}
+
+	Player getValue() { return this->p; }
+};
+
+class Lista
+{
+private:
+	Celula *primeiro;
+	Celula *ultimo;
+
+public:
+	Lista()
+	{
+		primeiro = nullptr;
+		ultimo = primeiro;
+	}
+
+	void InserirInicio(Player p)
+	{
+		cout << "Inserindo player " << p.getID() << endl;
+
+		Celula *nova = new Celula(p);
+		nova->prev = primeiro;
+		nova->next = ultimo;
+		primeiro->next = ultimo;
+
+		if (primeiro == ultimo)
+			ultimo = nova;
+		else
+			nova->next->prev = nova;
+
+		nova = nullptr;
+	}
+
+	void Mostrar()
+	{
+		Celula *aux = primeiro;
+
+		cout << "[ ";
+		while (aux != nullptr)
+		{
+			cout << aux->getValue().getID() << ": " << aux->getValue().getName()
+				  << ", ";
+			aux = aux->next;
+		}
+		cout << " ]" << endl;
 	}
 };
--- sysbase/main.cpp
+++ sysbase/main.cpp
@@ -2,14 +2,31 @@
 #include <cmath>
 
 #include "geo.cpp"
+#include "character.cpp"
 
 using namespace std;
 
 int main()
 {
-	Geo sanFrancisco(37.7749, -122.4194);
-	Geo newYork(40.7128, -74.0059);
-	cout << "San Francisco to New York: " << newYork.calcAngle(sanFrancisco) << "°" << endl
-		  << "New York to San Francisco: " << sanFrancisco.calcAngle(newYork) << "°" << endl;
+	// Geo sanFrancisco(37.7749, -122.4194);
+	// Geo newYork(40.7128, -74.0059);
+	// cout << "San Francisco to New York: " << newYork.calcAngle(sanFrancisco) << "°" << endl
+	// 	  << "New York to San Francisco: " << sanFrancisco.calcAngle(newYork) << "°" << endl;
+
+
+	Lista *l = new Lista();
+	l->Mostrar();
+
+	Player p;
+	cout << "Digite o nome do player: ";
+	string name;
+	cin >> name;
+	p.setName(name);
+	l->InserirInicio(p);
+	l->Mostrar();
+
+	// l->InserirInicio(p);
+	// l->Mostrar();
+
 	return 0;
 }
